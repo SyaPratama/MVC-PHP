a5f9a55b6b2ea79bc91c087de3ed2b935e173e5f
SyaPratama <inirasya16@gmail.com>
2024-11-14 07:34:23 +0700

    core: add env

--- app/Routes/Routes.php
+++ app/Routes/Routes.php
@@ -2,6 +2,8 @@
 
 use MVC\App\Core\Route;
 use MVC\App\Controller\Test;
+
+
 Route::add("GET","/hello/{id}/test/{idTest}",Test::class,"index");
 Route::add("GET","/hello",Test::class,"hello");
 Route::run();
